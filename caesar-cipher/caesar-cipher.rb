92f0504f04b0c9421550752b8204c45217428da7
dongchijoseph <huyblogging@gmail.com>
2021-06-09 09:03:35 +0700

    add more functions

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -12,15 +12,17 @@ def encrypt(character, number)
   num_array = Array("0".."9")
   alt_array = Array("a".."z")
   alt_array_upcase = Array("A".."Z")
-  symbol_array = " !\"\#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("")
+  symbol_array = Array('!'..'?') - Array("0".."9") 
 
   if num_array.include? character
-    (((character.to_i) + number) % 10).to_s
+#    (((character.to_i) + number) % 10).to_s
+    shift_factor(num_array, character, number)
   elsif alt_array.include? character
     shift_factor(alt_array, character, number)
   elsif alt_array_upcase.include? character
     shift_factor(alt_array_upcase, character, number)
-
+  elsif symbol_array.include? character
+    shift_factor(symbol_array, character, number)
   elsif character == " "
     character
   end
@@ -29,4 +31,4 @@ end
 def shift_factor(array, character, number)
     return array[((array.find_index(character) + number) % array.size)]
 end
-puts caesar_cipher("a 3bc5de", 1)
+puts caesar_cipher("What a string! 123", 5)
