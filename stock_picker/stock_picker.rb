bbb3bfbad8fd66eff8123c1f97a30d1c9a3a05d5
dongchijoseph <huyblogging@gmail.com>
2021-06-10 13:08:21 +0700

    some fixs - stock_picker

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,11 +1,5 @@
-require 'pry'
-
-def stock_picker(arr)
-  calc_profit(arr).max_by{|k,v| v}[0]
-end
-
-def calc_profit(arr) 
-  arr.reduce({}) do |result, price|
+def stock_picker(arr) 
+  pairs = arr.reduce({}) do |result, price|
     buy_day = arr.find_index(price)
     new_arr = arr[(buy_day + 1)..-1].select { |value| value > price }
     if new_arr != []
@@ -18,6 +12,7 @@ def calc_profit(arr)
       result
     end
   end
+    pairs.max_by{|k,v| v}[0]
 end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10])
