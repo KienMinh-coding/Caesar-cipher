f055ef70d16f8db001bc695f5c22ae977be37868
dongchijoseph <huyblogging@gmail.com>
2021-06-10 12:59:31 +0700

    add functions - stock_picker

--- stock_picker/stock_picker.rb
+++ stock_picker/stock_picker.rb
@@ -1,9 +1,23 @@
 require 'pry'
 
 def stock_picker(arr)
-  lowest_day = arr.find_index(arr.min)
-  new_arr = arr.drop(lowest_day)
+  calc_profit(arr).max_by{|k,v| v}[0]
 end
 
+def calc_profit(arr) 
+  arr.reduce({}) do |result, price|
+    buy_day = arr.find_index(price)
+    new_arr = arr[(buy_day + 1)..-1].select { |value| value > price }
+    if new_arr != []
+      sell_price = new_arr.max
+      profit = sell_price - price
+      sell_day = arr.find_index(sell_price)
+      result[[buy_day, sell_day]] = profit
+      result
+    else
+      result
+    end
+  end
+end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10])
