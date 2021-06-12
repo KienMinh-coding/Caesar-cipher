5d1de986d40dfd583b099fe39d8ad33d1bbf73a5
dongchijoseph <huyblogging@gmail.com>
2021-06-12 11:12:10 +0700

    some fixs

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,9 +1,5 @@
 def bubble_sort(arr)
-    if check(arr) == true
-      return arr
-    else
-      swapping(arr)
-    end
+  check(arr) == true ? arr : swapping(arr)  
 end
 
 def swapping(arr)
@@ -19,8 +15,16 @@ def swapping(arr)
 end
 
 def check(arr)
+  result = false
+  for i in 0..(arr.size - 2)
+    if arr[i] <= arr[i+1]
+      result = true
+    else
+      result = false
+      break
+    end
+  end
   result
-  arr.each
 end
 
-bubble_sort([4,3,78,2,0,2])
+puts bubble_sort([10, 2, 6, 2, 1, 5, 8, 6, 0, 2, 8, 9])
